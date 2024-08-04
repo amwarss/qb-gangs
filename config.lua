3a5820a602fba61eb7c8cf58110b3c7bc254d710
d3 <89148697+amwarss@users.noreply.github.com>
2024-08-05 02:26:34 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,47 +1,68 @@
 Config = {}
 
-Config.UseTarget = "ox"  -- اختر النظام الذي تريد استخدامه: "ox" أو "qb" أو "orbit"
+Config.UseTarget = "orbit"  -- اختر النظام الذي تريد استخدامه: "ox" أو "qb" أو "orbit"
+
+Config.vehicleMenuEnabled = true  -- تمكين أو تعطيل منطقة استخراج السيارات
+
+Config.UseInventory = "new" -- new or old
+
 
 Config.bossmenu = {
-    ["lostmc"] = vector3(1973.24, 4632.02, 40.27),
-    ["vagos"] = vector3(344.67, -2022.14, 22.39),
-    ["families"] = vector3(-136.91, -1609.84, 35.03),
+    ["vagos"] = {coords = vector3(1973.24, 4632.02, 40.27), type = "gang"},
+    -- ["vagos"] = {coords = vector3(344.67, -2022.14, 22.39), type = "gang"},
+    -- ["police"] = {coords = vector3(441.34, -981.85, 30.69), type = "job"},
 }
 
 Config.stash = {
-    ["ballas"] = vector3(112.0, -1970.0, 21.0),
-    ["vagos"] = vector3(343.0, -2021.0, 22.0),
-    ["lostmc"] = vector3(1970.08, 4634.33, 40.23),
+    ["vagos"] = {coords = vector3(1967.24, 4633.91, 41.09), type = "gang"},
+    -- ["vagos"] = {coords = vector3(343.0, -2021.0, 22.0), type = "gang"},
+    -- ["police"] = {coords = vector3(451.7, -973.5, 30.69), type = "job"},
 }
 
 Config.clothingMenu = {
-    ["lostmc"] = vector3(1971.79, 4621.63, 40.18),
-    ["vagos"] = vector3(299.3, -598.4, 43.3)
+    ["vagos"] = {coords = vector3(1959.16, 4627.73, 41.24), type = "gang"},
+    -- ["vagos"] = {coords = vector3(299.3, -598.4, 43.3), type = "gang"},
+    -- ["police"] = {coords = vector3(461.9, -998.6, 30.69), type = "job"},
 }
 
 Config.Stashes = {
-    ["lostmc"] = {
-        stashName = "lostmcstash",
+    ["vagos"] = {
+        stashName = "vagosstash",
         maxweight = 4000000,
         slots = 500,
+        type = "gang"
     },
-    ["gang2"] = {
-        stashName = "gang2stash",
+    ["police"] = {
+        stashName = "policestash",
         maxweight = 3000000,
         slots = 400,
+        type = "job"
     },
 }
 
 Config.personalStash = {
-    ["ballas"] = vector3(114.0, -1971.0, 21.0),
-    ["vagos"] = vector3(345.0, -2023.0, 22.0),
-    ["lostmc"] = vector3(1927.26, 4621.08, 39.65),
-    
+    ["vagos"] = {coords = vector3(1923.87, 4620.94, 39.92), type = "gang"},
+    -- ["vagos"] = {coords = vector3(345.0, -2023.0, 22.0), type = "gang"},
+    -- ["police"] = {coords = vector3(471.6, -991.2, 30.69), type = "job"},
 }
 
-Config.GangRanks = {
+Config.vehicleMenu = {
+    ["vagos"] = {
+        coords = vector3(1954.45, 4646.42, 39.72),
+        type = "gang",
+        vehicles = {"buccaneer2", "chino", "hermes"},
+        spawnCoords = vector4(1956.21, 4649.93, 40.73, 261.61),
+        npcCoords = vector4(1954.45, 4646.42, 39.72, 243.08),
+        npcModel = "s_m_y_cop_01",
+        returnCoords = vector3(1956.21, 4649.93, 40.73)
+    },
+}
+
+
+
+Config.GroupRanks = {
     ["ballas"] = true,
     ["vagos"] = true,
-    ["families"] = true,
+    ["police"] = true,
     ["lostmc"] = true,
 }
