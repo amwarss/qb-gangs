71f251097b3ff0ba30a184c9df02bb86348c89fe
d3 <89148697+amwarss@users.noreply.github.com>
2024-11-15 01:38:15 +0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'D3 Code Scripts - discord.gg/xG9FM2VjjY'
-description 'Code-Gang for new inspired 4.0'
+description 'code-groups for new inspired 4.0'
 
 
 shared_scripts {
